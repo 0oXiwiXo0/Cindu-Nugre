f6bb4d8a521442864423f73a1593cd197efc715d
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-08-26 23:02:42 +0200

    MAJ de .env.dev & services.yaml et limitations du nb de photo par page

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PhotoController extends AbstractController
 {
-    #[Route('/portfolio', name: 'photo_portfolio')]
-    public function portfolio(): Response
+    #[Route('/portfolio/{page}', name: 'photo_portfolio', requirements: ['page' => '\d+'], defaults: ['page' => 1], methods: ['GET'])]
+    public function portfolio(int $page, ParameterBagInterface $parameters, PhotoRepository $photoRepository): Response
     {
-        return $this->render('photo/portfolio.html.twig');
+        $nbPerPage = $parameters->get('photo')['nb_max'];
+        $offset = ($page - 1) * $nbPerPage;
+        $photos = $photoRepository->findBy([], null, $nbPerPage, $offset);
+        $total = $photoRepository->count([]);
+        $totalPages = ceil($total / $nbPerPage);
+
+        return $this->render('photo/portfolio.html.twig', [
+                'photos' => $photos,
+                'page' => $page,
+                'total_pages' => $totalPages,
+            ]
+        );
     }
 }
