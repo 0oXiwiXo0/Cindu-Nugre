f163663784eab473fec3d5d3b50cf189be39dbb3
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-09-03 22:31:43 +0200

    retrait bouton isPublished et harminisation du formulaire de contact.html.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -22,8 +24,23 @@ final class MainController extends AbstractController
     }
 
     #[Route('contact', name: 'contact', methods: ['GET'])]
-    public function contact(): Response
+    public function contact(Request $request): Response
     {
-        return $this->render('main/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            // ⚡ Ici tu pourrais envoyer un email avec MailerInterface
+            // Pour l'instant on affiche juste un flash message
+            $this->addFlash('success', 'Merci ' . $data['name'] . ', votre message a bien été envoyé !');
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('main/contact.html.twig', [
+            'contactForm' => $form->createView(),
+        ]);
     }
 }
